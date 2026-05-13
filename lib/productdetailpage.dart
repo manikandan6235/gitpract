05cce06e4acb2f23580d1e7b2d506cbe79b27957
e18312 <e18312@ejindia.com>
2026-05-13 16:30:53 +0530

    produclistanddetailpage

---
+++ lib/productdetailpage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailpage extends StatelessWidget {
+  const ProductDetailpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
